07001c86c26d89aa43e7c5c9b921cb2b14c6741e
thecookieorg <marko.manojlovic.bg@gmail.com>
2016-06-09 21:51:31 -0700

    dashboards controller added, modified root for auth. user, added bootstrap theme

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   
 
+  get 'dashboards/index'
+
   devise_for :users
+
+  authenticated :user do
+    root 'dashboards#index', as: :authenticated_root
+  end
+
   root 'pages#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
