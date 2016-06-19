1487c4dee03d1cc93b4e1aa78385e564d3faa563
thecookieorg <marko@thecookie.org>
2016-06-19 10:09:28 -0700

    cart created, line_items added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160618224335) do
+ActiveRecord::Schema.define(version: 20160619164920) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -27,8 +27,9 @@ ActiveRecord::Schema.define(version: 20160618224335) do
   create_table "line_items", force: :cascade do |t|
     t.integer  "product_id"
     t.integer  "cart_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "quantity",   default: 1
   end
 
   add_index "line_items", ["cart_id"], name: "index_line_items_on_cart_id"
