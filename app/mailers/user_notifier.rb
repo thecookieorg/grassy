b6e9ef47071b64d85810083173d059358b9c2921
thecookieorg <marko@thecookie.org>
2016-06-26 09:39:15 -0700

    added owners email

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -23,7 +23,7 @@ class UserNotifier < ApplicationMailer
 	# send an email when a purchase has been made
 	def send_order_confirmation_to_grassy_owner(order)
 		#@user = user
-		grassy_owner = 'mmanojlovic@cesgroup.ca'
+		grassy_owner = 'hello@grassy.ca'
 		@order = order
 		mail(
 			:to => grassy_owner,
