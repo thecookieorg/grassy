ee5e356ec139a05e50ef3dcf1c31947251cc21d5
thecookieorg <marko@thecookie.org>
2016-06-29 12:13:20 -0700

    registrations_controller update

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -34,7 +34,7 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def account_update_params
-    params.require(:user).permit(:name, :email, :street_address, :unit_number, :buzzer_number, :city, :province, :postal_code, :telephone, :date_of_birth, :is_female, :password, :password_confirmation, :current_password, :latitude, :longitude)
+    params.require(:user).permit(:name, :email, :street_address, :unit_number, :buzzer_number, :city, :province, :postal_code, :telephone, :date_of_birth, :is_female, :password, :password_confirmation, :current_password)
   end
 
 
