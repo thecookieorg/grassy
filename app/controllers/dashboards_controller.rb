b51b3b4ecea7979867f90ba70642ea914b4e3a75
thecookieorg <marko@thecookie.org>
2016-07-29 10:33:35 -0700

    listed users id -> asc

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -6,14 +6,14 @@ class DashboardsController < ApplicationController
   	# I didn't actually need the code below. It raises an error => wrong number of arguments (given 1, expected 2+)
   	# I am not sure what that error is. authorize! :index works for products_controller, hmmmmmmmmm!
   	#authorize! :index 
-  	@users = User.all
+  	@users = User.all.order('id ASC')
   	@products = Product.all
     @orders = Order.all
     #@all_users_analytics = User.all
   end
 
   def list_all_users
-  	@users = User.all
+  	@users = User.all.order('id ASC')
     @orders = Order.all
   end
 
